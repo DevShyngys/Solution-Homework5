f663c47602b5ca617ee1cab21fd2e88601323683
DevShyngys <chingizbulatov72@gmail.com>
2025-04-04 20:28:37 +0500

    Added Protection Proxy to ensure login before image upload

---
+++ src/ImageUploaderProxy.java
@@ -0,0 +1,19 @@
+package src;
+
+public class ImageUploaderProxy extends ProxyImage {
+    private boolean userLoggedIn;
+
+    public ImageUploaderProxy(String filename, boolean userLoggedIn) {
+        super(filename);
+        this.userLoggedIn = userLoggedIn;
+    }
+
+    @Override
+    public void displayFullImage() {
+        if (!userLoggedIn) {
+            System.out.println("Access Denied. Please log in to view high-res images.");
+        } else {
+            super.displayFullImage();
+        }
+    }
+}
